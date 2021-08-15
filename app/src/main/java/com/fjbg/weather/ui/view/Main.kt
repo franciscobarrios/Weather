d839e589445aaead81b5aaa486a69d25fa9c8926
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-08-15 10:08:11 +0700

    Preparing UI

--- app/src/main/java/com/fjbg/weather/ui/view/Main.kt
+++ app/src/main/java/com/fjbg/weather/ui/view/Main.kt
@@ -1,28 +1,36 @@
 package com.fjbg.weather.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 
 @Composable
 fun MainView() {
     Box(
         Modifier
-            .background(Color.Black)
+            .background(Color.White)
             .fillMaxSize()
     ) {
         Column(
-            Modifier
+            modifier = Modifier
                 .padding(16.dp)
                 .background(Color.White)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center
         ) {
             PlaceName()
+            WeatherIcon()
+            WeatherInfo()
         }
     }
 }
@@ -32,31 +40,71 @@ fun PlaceName() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(16.dp),
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Thailand,",
-                fontSize = 12.sp,
-                color = Color.Gray,
+                text = "Bangkok, Thailand",
+                fontSize = 14.sp,
+                color = Color.DarkGray,
             )
         }
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Center
 
         ) {
             Text(
-                text = "Bangkok",
-                fontSize = 18.sp,
-                color = Color.DarkGray,
+                text = "Sun 15 Aug",
+                fontSize = 12.sp,
+                color = Color.Gray,
             )
         }
+    }
+}
+
+@Composable
+fun WeatherIcon() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberImagePainter("icon_weather.xml"),
+            contentDescription = null,
+            modifier = Modifier.size(148.dp).fillMaxWidth(),
+        )
+    }
+}
+
+@Composable
+fun WeatherInfo() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Text(
+            text = "28°",
+            fontSize = 98.sp,
+            color = Color.Gray,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "Raining",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
